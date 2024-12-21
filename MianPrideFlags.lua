9f4983eda63b4175827017a85e69ae4137bb6402
Mian <docgoosenn@gmail.com>
2024-12-21 13:57:53 -0800

    - Edited since the framework adds flags differently now

--- MianPrideFlags.lua
+++ MianPrideFlags.lua
@@ -2,7 +2,7 @@ local mutatorName = "MianPrideFlags"
 behaviour(mutatorName)
 
 function MianPrideFlags:Awake()
-	self.frameworkName = "Flag Framework(Clone)"
+	self.frameworkName = "Custom Flag Framework"
 	self.dataContainer = self.gameObject.GetComponent(DataContainer)
 	self.CustomFlags = self.dataContainer.GetTextureArray("Flag")
 	self.CustomFlagTeamColors = self.dataContainer.GetColorArray("Flag")
@@ -17,12 +17,5 @@ function MianPrideFlags:Start()
 		error("Framework was not found! Please make sure the Custom Flags Framework mutator has been enabled.")
 	end
 
-	local mutatorData = {
-		name = mutatorName,
-		cover = self.cover
-	}
-
-	for index, texture in pairs(self.CustomFlags) do
-		self.framework:addTextureData(mutatorData, texture, self.CustomFlagTeamColors[index])
-	end
+	self.framework:addTexturePack(mutatorName, self)
 end
