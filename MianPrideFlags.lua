9637bc4844f43d9c92bdbca145f843f43104ad5c
MianReplicate <docgoosenn@gmail.com>
2026-07-18 21:36:12 -0700

    Updated pack with new framework standards

--- MianPrideFlags.lua
+++ MianPrideFlags.lua
@@ -2,7 +2,6 @@ local mutatorName = "MianPrideFlags"
 behaviour(mutatorName)
 
 function MianPrideFlags:Awake()
-	self.frameworkName = "Custom Flag Framework"
 	self.dataContainer = self.gameObject.GetComponent(DataContainer)
 	self.additionalDataContainer = self.targets.AdditionalDataContainer.GetComponent(DataContainer)
 	self.CustomFlags = self.dataContainer.GetTextureArray("Flag")
@@ -18,15 +17,4 @@ function MianPrideFlags:Awake()
 
 	self.cover = self.dataContainer.GetTexture("Cover")
 	self.name = mutatorName
-end
-
-function MianPrideFlags:Start()
-	local obj = GameObject.Find(self.frameworkName)
-	if(obj) then
-		self.framework = ScriptedBehaviour.GetScript(obj)
-	else
-		error("Framework was not found! Please make sure the Custom Flags Framework mutator has been enabled.")
-	end
-
-	self.framework:addFlagPack(self)
 end
